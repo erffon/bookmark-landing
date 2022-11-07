fc64b5a2560123633246403314558489af201582
office <erffon@gmail.com>
2022-11-07 11:56:41 +0330

    dark mode config set on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
+  darkMode: "media",
   theme: {
     container: {
       center: true,
